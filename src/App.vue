b7cd112c2d760366ff6eaa4152458a842dd64079
Fabien Grignoux <fabien.grignoux@outlook.com>
2022-06-01 19:40:28 +0200

    update package json for build

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="my-4 mx-10">
-    <h1 class="text-4xl">Podia Visualizer</h1>
+    <h1 class="text-4xl">Podia Analytics</h1>
     <router-view></router-view>
   </div>
 </template>
